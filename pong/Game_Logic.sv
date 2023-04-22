d5fd1209f47316fc5ccfa953194d9e50e60c2052
unknown <edziem2@illinois.edu>
2023-04-22 14:25:44 -0500

    pieces switch now

--- pong/Game_Logic.sv
+++ pong/Game_Logic.sv
@@ -23,9 +23,10 @@ module Game_Logic (
 	assign Pieces = '{
 		'{6'b000000,6'b001000,6'b001001,6'b010001}, // piece 1
 		'{6'b000000,6'b000001,6'b001000,6'b001001}, // piece 2
-		'{6'b000000,6'b000000,6'b000000,6'b000000}  // piece 3
+		'{6'b000000,6'b000001,6'b000010,6'b000011}  // piece 3
 	}; // array of 3 different peices. each register in the array contains the coordinates
 	// for a block of the peice in the format [5:3]=Y [2:0]=X, relative to (0,0)
+	logic [2:0] piece_count;
 	
 	logic move_clk;
 	logic [5:0] frame_count;
@@ -54,15 +55,16 @@ module Game_Logic (
         begin 
             blockXMotion <= 7'd0;
 				blockYMotion <= 8'd0; 
-				blockX1 <= Pieces[1][0][2:0] + (board_width>>1); // divided by 2
-				blockY1 <= Pieces[1][0][5:3];
-				blockX2 <= Pieces[1][1][2:0] + (board_width>>1);
-				blockY2 <= Pieces[1][1][5:3];
-				blockX3 <= Pieces[1][2][2:0] + (board_width>>1);
-				blockY3 <= Pieces[1][2][5:3];
-				blockX4 <= Pieces[1][3][2:0] + (board_width>>1);
-				blockY4 <= Pieces[1][3][5:3];
-
+				blockX1 <= Pieces[0][0][2:0] + (board_width>>1); // divided by 2
+				blockY1 <= Pieces[0][0][5:3];
+				blockX2 <= Pieces[0][1][2:0] + (board_width>>1);
+				blockY2 <= Pieces[0][1][5:3];
+				blockX3 <= Pieces[0][2][2:0] + (board_width>>1);
+				blockY3 <= Pieces[0][2][5:3];
+				blockX4 <= Pieces[0][3][2:0] + (board_width>>1);
+				blockY4 <= Pieces[0][3][5:3];
+				
+				piece_count <= 0;
 				color <= 1;
 				Board <= '{default: 16'h0};
 
@@ -103,15 +105,19 @@ module Game_Logic (
 					Board[blockY2][blockX2] <= 1'b1;
 					Board[blockY3][blockX3] <= 1'b1;
 					Board[blockY4][blockX4] <= 1'b1;
-					blockX1 <= Pieces[0][0][2:0] + (board_width>>1); // divided by 2
-					blockY1 <= Pieces[0][0][5:3];
-					blockX2 <= Pieces[0][1][2:0] + (board_width>>1); // divided by 2
-					blockY2 <= Pieces[0][1][5:3];
-					blockX3 <= Pieces[0][2][2:0] + (board_width>>1); // divided by 2
-					blockY3 <= Pieces[0][2][5:3];
-					blockX4 <= Pieces[0][3][2:0] + (board_width>>1); // divided by 2
-					blockY4 <= Pieces[0][3][5:3];
+					blockX1 <= Pieces[piece_count][0][2:0] + (board_width>>1); // divided by 2
+					blockY1 <= Pieces[piece_count][0][5:3];
+					blockX2 <= Pieces[piece_count][1][2:0] + (board_width>>1); // divided by 2
+					blockY2 <= Pieces[piece_count][1][5:3];
+					blockX3 <= Pieces[piece_count][2][2:0] + (board_width>>1); // divided by 2
+					blockY3 <= Pieces[piece_count][2][5:3];
+					blockX4 <= Pieces[piece_count][3][2:0] + (board_width>>1); // divided by 2
+					blockY4 <= Pieces[piece_count][3][5:3];
 					color <= color+1;
+					if (piece_count == 3'h2)
+						piece_count <= 0;
+					else
+						piece_count <= piece_count + 1;
 				end
 				else begin
 					blockX1 <= blockX1 + blockXMotion;
