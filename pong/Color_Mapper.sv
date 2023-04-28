e63806e3fb314eaa5f4537177179262f8e2c56a6
unknown <edziem2@illinois.edu>
2023-04-27 19:47:18 -0500

    working on new color mapper for sdram

--- pong/Color_Mapper.sv
+++ pong/Color_Mapper.sv
@@ -14,68 +14,147 @@
 
 // VGA = 640*480
 
-module  color_mapper ( input        [9:0] blockx1, blocky1, blockx2, blocky2, blockx3, blocky3, blockx4, blocky4, DrawX, DrawY, Ball_size,
-                       output logic [7:0]  Red, Green, Blue );
+module  color_mapper (  input logic [15:0] Row [10],
+								input logic rowReady,
+								input logic [9:0] DrawX, DrawY,
+								output logic [7:0] rowNum,
+							   output logic LD_Row,
+								output logic [7:0]  Red, Green, Blue );
     
 	 
-    logic ball_on;
-	 
-
-	 parameter squareSize = (640/2)/12; // half of the screen is the board, and divide that by
+	 parameter squareSize = (640/2)/(board_width+1); // half of the screen is the board, and divide that by
 	 // the # of squares in a row of the board
-	 logic [9:0] BallX [4]; 
-	 logic [9:0] BallY [4];
+	 parameter [6:0] board_width =9; // number of squares in each row (starting at 0)
+	 parameter [6:0] board_height =19;
 	 
-	 assign BallX[0] = (blockx1 * squareSize) + (640/4); // start at second quarter of screen
-	 assign BallY[0] = blocky1 * squareSize;
-	 assign BallX[1] = (blockx2 * squareSize) + (640/4); // start at second quarter of screen
-	 assign BallY[1] = blocky2 * squareSize;
-	 assign BallX[2] = (blockx3 * squareSize) + (640/4); // start at second quarter of screen
-	 assign BallY[2] = blocky3 * squareSize;
-	 assign BallX[3] = (blockx4 * squareSize) + (640/4); // start at second quarter of screen
-	 assign BallY[3] = blocky4 * squareSize;
-//	 assign BallX = ballx; // start at second quarter of screen
-//	 assign BallY = bally;
-    int DistX1, DistY1, DistX2, DistY2, DistX3, DistY3, DistX4, DistY4;
-	 int Size;
-	 assign DistX1 = DrawX - BallX[0]; 
-    assign DistY1 = DrawY - BallY[0]; 
-	 assign DistX2 = DrawX - BallX[1]; 
-    assign DistY2 = DrawY - BallY[1]; 
-	 assign DistX3 = DrawX - BallX[2]; 
-    assign DistY3 = DrawY - BallY[2]; 
-	 assign DistX4 = DrawX - BallX[3]; 
-    assign DistY4 = DrawY - BallY[3]; 
-//    assign Size = Ball_size;
-	assign Size = squareSize/2;
-	  
-    always_comb
-    begin:Ball_on_proc
-        if ( 
-		  (DistX1*DistX1+DistY1*DistY1)<=(Size*Size) || (DistX2*DistX2+DistY2*DistY2)<=(Size*Size) || 
-		  (DistX3*DistX3+DistY3*DistY3)<=(Size*Size) || (DistX4*DistX4+DistY4*DistY4)<=(Size*Size) || 
-		  (DrawX <= (640/4) - (squareSize/2)) || (DrawX >= (3*640/4) + (squareSize/2))
-		  ) 
-            ball_on = 1'b1;
-        else 
-            ball_on = 1'b0;
-     end 
+	 logic [7:0] currentRow;
+	 
        
     always_comb
     begin:RGB_Display
-        if ((ball_on == 1'b1)) 
-        begin 
-            Red = 8'hff;
-            Green = 8'h55;
-            Blue = 8'h00;
-        end  
-        else 
+        if ((DrawX >= (640/4)) && (DrawX < (3*640/4)))  // if drawing in board
         begin 
             Red = 8'h00; 
             Green = 8'h00;
-            Blue = 8'h7f - DrawX[9:3];
+            Blue = 8'h7c;
+        end  
+        else 
+        begin // draw side bars
+            Red = 8'h00; 
+            Green = 8'h00;
+            Blue = 8'h7c;
         end   
-
     end 
     
+	 
+	 enum logic [15:0] {S1, S2, Wait} state;
+
+	// State machine logic with reset for correct default values of regs
+	always_ff @(posedge clk)
+	begin
+		unique case (state)
+			Wait: begin
+				if (hs == 1'b1 && ((DrawY+1) % squareSize == 0)) begin // at the end of a block row fetch the next row
+					state <= S1
+				end
+				end 
+			S1: begin
+				state <= Wait
+				end 
+//			S2: begin
+//				
+//				state <= Wait
+//				end 
+	end
+	
+	always_comb 
+	begin
+		rowNum = 0;
+		LD_Row = 1'b0;
+		unique case (state)
+			Wait: begin
+				if (DrawY >= 639) begin  // if at the end of the last block row
+					rowNum = 0;
+					LD_Row = 1'b1;
+				end
+				else if (hs == 1'b1 && ((DrawY+1) % squareSize == 0)) begin  // else if at the end at a block row
+					rowNum = (DrawY+1) / squareSize; // get the next block row
+					LD_Row = 1'b1;
+				end
+			end
+			S1: begin  // send load signal
+				LD_Row = 1'b0;
+				end 
+//			S2: begin
+//				LD_Row = 1'b0;
+//				end 
+	end
 endmodule
+
+//// VGA = 640*480
+//
+//module  color_mapper ( input        [9:0] blockx1, blocky1, blockx2, blocky2, blockx3, blocky3, blockx4, blocky4, DrawX, DrawY, Ball_size,
+//                       output logic [7:0]  Red, Green, Blue );
+//    
+//	 
+//    logic ball_on;
+//	 
+//
+//	 parameter squareSize = (640/2)/12; // half of the screen is the board, and divide that by
+//	 // the # of squares in a row of the board
+//	 logic [9:0] BallX [4]; 
+//	 logic [9:0] BallY [4];
+//	 
+//	 assign BallX[0] = (blockx1 * squareSize) + (640/4); // start at second quarter of screen
+//	 assign BallY[0] = blocky1 * squareSize;
+//	 assign BallX[1] = (blockx2 * squareSize) + (640/4); // start at second quarter of screen
+//	 assign BallY[1] = blocky2 * squareSize;
+//	 assign BallX[2] = (blockx3 * squareSize) + (640/4); // start at second quarter of screen
+//	 assign BallY[2] = blocky3 * squareSize;
+//	 assign BallX[3] = (blockx4 * squareSize) + (640/4); // start at second quarter of screen
+//	 assign BallY[3] = blocky4 * squareSize;
+////	 assign BallX = ballx; // start at second quarter of screen
+////	 assign BallY = bally;
+//    int DistX1, DistY1, DistX2, DistY2, DistX3, DistY3, DistX4, DistY4;
+//	 int Size;
+//	 assign DistX1 = DrawX - BallX[0]; 
+//    assign DistY1 = DrawY - BallY[0]; 
+//	 assign DistX2 = DrawX - BallX[1]; 
+//    assign DistY2 = DrawY - BallY[1]; 
+//	 assign DistX3 = DrawX - BallX[2]; 
+//    assign DistY3 = DrawY - BallY[2]; 
+//	 assign DistX4 = DrawX - BallX[3]; 
+//    assign DistY4 = DrawY - BallY[3]; 
+////    assign Size = Ball_size;
+//	assign Size = squareSize/2;
+//	  
+//    always_comb
+//    begin:Ball_on_proc
+//        if ( 
+//		  (DistX1*DistX1+DistY1*DistY1)<=(Size*Size) || (DistX2*DistX2+DistY2*DistY2)<=(Size*Size) || 
+//		  (DistX3*DistX3+DistY3*DistY3)<=(Size*Size) || (DistX4*DistX4+DistY4*DistY4)<=(Size*Size) || 
+//		  (DrawX <= (640/4) - (squareSize/2)) || (DrawX >= (3*640/4) + (squareSize/2))
+//		  ) 
+//            ball_on = 1'b1;
+//        else 
+//            ball_on = 1'b0;
+//     end 
+//       
+//    always_comb
+//    begin:RGB_Display
+//        if ((ball_on == 1'b1)) 
+//        begin 
+//            Red = 8'hff;
+//            Green = 8'h55;
+//            Blue = 8'h00;
+//        end  
+//        else 
+//        begin 
+//            Red = 8'h00; 
+//            Green = 8'h00;
+//            Blue = 8'h7f - DrawX[9:3];
+//        end   
+//
+//    end 
+//    
+//endmodule
--- pong/lab62.sv
+++ pong/lab62.sv
@@ -231,13 +231,19 @@ logic [6:0] blockYPos [4];
 logic [6:0] blockXPrev [4];
 logic [6:0] blockYPrev [4];
 logic [3:0] blockColor;
+logic LD_Row, rowReady;
+
+logic [15:0] Row [10];
+logic [7:0] rowNum;
 							
 tetris tet (.*, .clk(MAX10_CLK1_50), .vs(VGA_VS), .hs(VGA_HS), .reset(Reset_h), .DrawX(drawxsig), .DrawY(drawysig), .Red(), .Green(), .Blue()); // Tetris instantiation
 
 Game_Logic game (.Reset(Reset_h), .frame_clk(~VGA_VS), .keycode(keycode), .blockXPos(blockXPos), .blockYPos(blockYPos), .blockXPrev(blockXPrev), .blockYPrev(blockYPrev), .blockColor(blockColor));
 
-//color_mapper colormap (.blockx1(blockx1sig), .blocky1(blocky1sig), .blockx2(blockx2sig), .blocky2(blocky2sig), .blockx3(blockx3sig), 
-//	.blocky3(blocky3sig), .blockx4(blockx4sig), .blocky4(blocky4sig), .DrawX(drawxsig), .DrawY(drawysig), .Ball_size(ballsizesig), .Red(Red), .Green(Green), .Blue(Blue));
+//color_mapper colormap (.blockx1(blockXPos[0]), .blocky1(blockYPos[0]), .blockx2(blockXPos[1]), .blocky2(blockYPos[1]), .blockx3(blockXPos[2]), 
+//	.blocky3(blockYPos[2]), .blockx4(blockXPos[3]), .blocky4(blockYPos[3]), .DrawX(drawxsig), .DrawY(drawysig), .Ball_size(ballsizesig), .Red(Red), .Green(Green), .Blue(Blue));
+
+color_mapper colormap (.LD_Row(LD_Row), .rowReady(rowReady), .Row(Row), .rowNum(rowNum), .DrawX(drawxsig), .DrawY(drawysig), .Ball_size(ballsizesig), .Red(Red), .Green(Green), .Blue(Blue));
 
 vga_controller vga_ctrl (.Clk(MAX10_CLK1_50), .Reset(1'b0), .hs(VGA_HS), .vs(VGA_VS), .pixel_clk(), .blank(), .sync(), .DrawX(drawxsig), .DrawY(drawysig));
 
