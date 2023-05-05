e24dc5278551c19437bce83bc3d6f2a2a5e75da1
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-05-04 19:05:37 -0500

    I should be banned from git for all these commits

--- pong/Color_Mapper.sv
+++ pong/Color_Mapper.sv
@@ -216,9 +216,9 @@ module  color_mapper (  input Clk, hs, reset, frame_clk,
 				if(DrawX < ((right_edge)+8*1))
 						sprite_addr = (({1'b0, DrawY} - (16 + pointer)) + 16*(11'h2b)); // Code for 0 is x30
 				else if(DrawX < ((right_edge)+8*2))
-						sprite_addr = (({1'b0, DrawY} - (pointer - 16)) + 16*(11'h31)); // Code for 1 is x31
+						sprite_addr = (({1'b0, DrawY} - (16 + pointer)) + 16*(11'h31)); // Code for 1 is x31
 				else if(DrawX < ((right_edge)+8*3))
-						sprite_addr = (({1'b0, DrawY} - (pointer - 16)) + 16*(11'h30)); // Code for 1 is x31
+						sprite_addr = (({1'b0, DrawY} - (16 + pointer)) + 16*(11'h30)); // Code for 1 is x31
 		  
 		  		if(sprite_data[3'd7 - (((DrawX - (right_edge)) % 8))] == 1'b1)
 						begin
